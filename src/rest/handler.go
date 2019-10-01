fe1db9e9e22b4c479d55a5194ba3c45e4e6ec7a4
Alexander Hartson <alexander-hartson@live.com>
2019-10-01 14:44:28 -0500

    finishing hander.go

--- src/rest/handler.go
+++ src/rest/handler.go
@@ -117,3 +117,29 @@ func (h *Handler) SignOut(c *gin.Context) {
 	}
 }
 
+func (h *Handler) GetOrders(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+	// get id parameter
+	p := c.Param("id")
+	// conver the string 'p' to integer 'id'
+	id, err := strconv.Atoi(p)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	// call the database layer method to get orders from id
+	orders, err := h.db.GetCustomerOrdersByID(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, orders)
+}
+
+func (h *Handler) Charge(c *gin.Context) {
+	if h.db == nil {
+		return
+	}
+}
